69f3d84bbfd9b319b060e9da333310f9aa6c30dc
ThundaHorse <abeeeeekim@gmail.com>
2020-02-12 17:22:51 -0500

    added logos

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,25 @@
+<template>
+  <img class="nuxt-logo" src="../static/nuxt-logo.png" alt="Nuxt Logo" />
+</template>
+
+<script>
+  export default {
+    data() {
+      return {};
+    }
+  };
+</script>
+
+<style>
+  .nuxt-logo {
+    width: 180px;
+    transform: rotateY(560deg);
+    animation: turn 3.5s ease-out forwards 1s;
+  }
+
+  @keyframes turn {
+    100% {
+      transform: rotateY(0deg);
+    }
+  }
+</style>
